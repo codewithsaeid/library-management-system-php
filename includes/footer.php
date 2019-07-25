ecda4c9326535e846b03dd42fbdf04b443d71e1a
muhammadsaeid <muhammadsaeid0@gmail.com>
2019-07-26 04:19:26 +0600

    fix footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                   &copy; 2018 Online Library Management System |<a href="https://www.linkedin.com/in/aleksandart96/" target="_blank"  > Designed by : Aleksandar Trifunovic</a> 
+                   &copy; 2019 Online Library Management System |<a href="https://www.linkedin.com/in/jahidulsaeid" target="_blank"  > Designed by : Saeid</a> 
                 </div>
 
             </div>
